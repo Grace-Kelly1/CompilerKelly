639c9532b69c4ee395f7a49711e50a6d51dd416c
Grace Kelly <Grace.Kelly1@Marist.edu>
2019-03-04 13:15:10 -0500

    Started Node.ts and working on Parse and cst files

---
+++ Project/Node.ts
@@ -0,0 +1,96 @@
+module TSCompiler{
+    export class Node{
+        private isLeaf: boolean;
+        private isBoolean: boolean = false;
+        private isInt: boolean = false;
+        private isId: boolean = false;
+        private type: string;
+        private value: string;
+        public children: Node[];
+        private parent: Node;
+        private lineNumber: number;
+
+        constructor(type?: string) {
+            if (type) {
+                this.type = type;
+            } else {
+                this.type = "";
+            }
+
+            this.value = "";
+            this.children = [];
+            this.parent = null;
+            this.lineNumber = 0;
+            this.isLeaf = false;
+        }
+
+        public getType(): string {
+            return this.type;
+        }
+
+        public setType(type: string): void {
+            this.type = type;
+        }
+
+        public getValue(): string {
+            return this.value;
+        }
+
+        public setValue(value: string): void {
+            this.value = value;
+        }
+
+        public getParent(): Node {
+            return this.parent;
+        }
+
+        public setParent(parent: Node): void {
+            this.parent = parent;
+        }
+
+        public getLineNumber(): number {
+            return this.lineNumber;
+        }
+
+        public setLineNumber(number: number): void {
+            this.lineNumber = number;
+        }
+
+        public checkLeaf(): boolean {
+            return this.isLeaf;
+        }
+
+        public setLeaf(bool: boolean): void {
+            this.isLeaf = bool;
+        }
+
+        public checkBoolean(): boolean {
+            return this.isBoolean;
+        }
+
+        public setBoolean(bool: boolean): void {
+            this.isBoolean = bool;
+        }
+
+        public getInt(): boolean {
+            return this.isInt;
+        }
+
+        public setInt(bool: boolean): void {
+            this.isInt = bool;
+        }
+
+        public addChild(node: Node): void {
+            this.children.push(node);
+        }
+
+        public getId(): boolean {
+            return this.isId;
+        }
+
+
+        public setId(bool: boolean): void {
+            this.isId = bool;
+        }
+    }
+}
--- Project/cst.ts
+++ Project/cst.ts
@@ -4,23 +4,25 @@ module TSCompiler{
         private currentNode: Node;
 
         constructor(){
-
+            this.root = null;
+            this.currentNode = null;
         }
 
         public addLeaf(){
 
         }
 
-        public addBranch(){
-
+        public addBranch(type: string): void{
+            var node: Node = new Node();
+            node.setType(type);
         }
 
-        public setRoot(){
-
+        public setRoot(node: Node){
+            this.root = node;
         }
 
-        public getRoot(){
-
+        public getRoot(): Node{
+            return this.root;
         }
 
         public endChildren(){
