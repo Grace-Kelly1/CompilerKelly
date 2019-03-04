2589a541bcdcdd9f9652e1f85d6e974909c6716a
Grace Kelly <Grace.Kelly1@Marist.edu>
2019-03-04 11:29:50 -0500

    Setting up Parse file

---
+++ Project/cst.ts
@@ -0,0 +1,30 @@
+module TSCompiler{
+    export class csTree{
+        private root: Node;
+        private currentNode: Node;
+
+        constructor(){
+
+        }
+
+        public addLeaf(){
+
+        }
+
+        public addBranch(){
+
+        }
+
+        public setRoot(){
+
+        }
+
+        public getRoot(){
+
+        }
+
+        public endChildren(){
+
+        }
+    }
+}
---
+++ Project/parse.ts
@@ -0,0 +1,75 @@
+/// <reference path="globals.ts"/>
+/// <reference path="logger.ts"/>
+/// <reference path="token.ts"/>
+/// <reference path="utils.ts"/>
+/// <reference path="lexer.ts"/>
+
+module TSCompiler{
+    export class Parser{
+        public parse(){
+
+        }
+
+        public parseProgram(){
+
+        }
+
+        public parseBlock(){
+
+        }
+
+        public  parseStatments(){
+
+        }
+
+        public parseStatmentL(){
+
+        }
+
+        public parsePrint(){
+
+        }
+
+        public parseAssign(){
+
+        }
+
+        public parseVar(){
+
+        }
+
+        public parseWhile(){
+
+        }
+
+        public parseIf(){
+
+        }
+
+        public parseExpr(){
+
+        }
+
+        public parseInt(){
+
+        }
+
+        public parseString(){
+
+        }
+
+        public parseBoolean(){
+
+        }
+
+        public parseId(){
+
+        }
+
+        public parseChar(){
+
+        }
+
+    }
+
+}
