010e3c4e9834c8f2557bc85606aba046f9679d12
Grace Kelly <Grace.Kelly1@Marist.edu>
2019-03-11 14:47:04 -0400

    Fixing how Parse Prints to log

---
+++ logger.js
@@ -0,0 +1,43 @@
+/// <reference path="globals.ts"/>
+/// <reference path="token.ts"/>
+/// <reference path="utils.ts"/>
+/// <reference path="Tree.ts"/>
+var TSCompiler;
+(function (TSCompiler) {
+    var logger = /** @class */ (function () {
+        function logger() {
+        }
+        logger.prototype.printMessage = function (message) {
+            var log = document.getElementById("outputTA");
+            log.value += message + "\n";
+        };
+        logger.prototype.printError = function (message) {
+            var log = document.getElementById("outputTA");
+            log.value += "ERROR: " + message + "\n";
+        };
+        logger.prototype.printWarning = function (message) {
+            var log = document.getElementById("outputTA");
+            log.value += "WARNING!!!: " + message + "\n";
+        };
+        logger.prototype.printCST = function () {
+            var log = document.getElementById('cst_output');
+            //Print code not tree???
+            _Tree_ = new TSCompiler.Tree();
+            log.value += TSCompiler.Tree.toString();
+        };
+        logger.prototype.printParseMessage = function (message) {
+            var log = document.getElementById("outputTA");
+            log.value += message + "\n";
+        };
+        logger.prototype.printParseError = function (message) {
+            var log = document.getElementById("outputTA");
+            log.value += "ERROR: " + message + "\n";
+        };
+        logger.prototype.printParseComplete = function () {
+            var log = document.getElementById("outputTA");
+            log.value += "Parse Completed" + "\n";
+        };
+        return logger;
+    }());
+    TSCompiler.logger = logger;
+})(TSCompiler || (TSCompiler = {}));
---
+++ utils.js
@@ -0,0 +1,43 @@
+///<reference path='globals.ts' />
+///<reference path='lexer.ts' />
+///<reference path='logger.ts' />
+///<reference path='token.ts' />
+var TSCompiler;
+(function (TSCompiler) {
+    var utils = /** @class */ (function () {
+        function utils() {
+        }
+        utils.compile = function () {
+            //test line
+            //console.log("HERE");
+            // Reset everything
+            //_Tokens_ = [];
+            _CurrentT_ = null;
+            _TokenIndex_ = 0;
+            _Lexer_ = new TSCompiler.lexer(); // We declared these in globals but still have 
+            _Log_ = new TSCompiler.logger(); // to construct them before we can use them.
+            _Parser_ = new TSCompiler.parse();
+            _Tree_ = new TSCompiler.Tree();
+            var log = document.getElementById("outputTA");
+            var source = document.getElementById("inputTA");
+            source.value = this.trim(source.value);
+            log.value = "";
+            if (source.value === '') {
+                _Log_.printMessage("Empty TextArea");
+                return;
+            }
+            _Lexer_.lexerCode();
+            // while (_TokenIndex_ < _Tokens_.length) {
+            //     _Parser_.parse();
+            //     _Log_.printMessage("Completed parsing program.");
+            // }
+            // _Log_.printCST();
+        };
+        // Used in some places but specifically typed out in others. (TODO: Be consistent about this.)
+        utils.trim = function (words) {
+            return words.replace(/^\s+ | \s+$/g, "");
+        };
+        return utils;
+    }());
+    TSCompiler.utils = utils;
+})(TSCompiler || (TSCompiler = {}));
